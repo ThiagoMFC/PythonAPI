1b4db321e59b3ef859b6cab505dd35d7f584d6cb
ThiagoMFC <24575360+ThiagoMFC@users.noreply.github.com>
2026-08-25 14:14:56 -0300

    Add Post model and /createposts endpoint for posts API
    Added a Pydantic Post model with title and content fields. Introduced a /createposts POST endpoint to accept and return post data. Made minor formatting and import updates. Updated PythonAPI.slnx with an Id for PythonAPI.pyproj.

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 #create instance of FastAPI named app
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
 
 #define a path GET operation (route/endpoint) decorator
 @app.get("/")
@@ -11,3 +17,9 @@ def root():
 @app.get("/posts")
 def get_posts():
     return{"data": "posts will go here"}
+
+@app.post("/createposts")
+#extract all fields from body, convert to dict, store inside payload
+def create_posts(new_post: Post):
+    return{"new_post":f"title: {new_post.title} content: {new_post.content}"}
+
