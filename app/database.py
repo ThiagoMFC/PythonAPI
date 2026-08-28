ff2002c23cb8c229b829b8818de10e020e8f480d
ThiagoMFC <24575360+ThiagoMFC@users.noreply.github.com>
2026-08-28 11:08:00 -0300

    Add database connection setup using SQLAlchemy ORM

---
+++ app/database.py
@@ -0,0 +1,15 @@
+#implement sqlalchemy ORM
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_USER_PWD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
