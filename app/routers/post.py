7dd5eef1329829e46e42529652cde41a599cc428
ThiagoMFC <24575360+ThiagoMFC@users.noreply.github.com>
2026-09-03 13:09:12 -0300

    Refactor post router to use current_user for authentication in endpoint functions

--- app/routers/post.py
+++ app/routers/post.py
@@ -7,13 +7,13 @@ from typing import List
 router = APIRouter(prefix="/posts")
 
 @router.get("/", response_model=List[schemas.PostResponse])
-def get_posts(db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     posts = db.query(models.Post).where(models.Post.published == True).all()
     return posts
 
 @router.post("/", status_code = status.HTTP_201_CREATED, 
           response_model=schemas.PostResponse)
-def create_posts(new_post: schemas.PostCreate, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def create_posts(new_post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
 
     #Using ORM sqlalchemy / unpack new_post dict into correct format
     new_p = models.Post(**new_post.model_dump())
@@ -36,7 +36,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
     return post
 
 @router.delete("/{id}", status_code = status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     post_query = db.query(models.Post).where(models.Post.id == id)
     post = post_query.first()
 
@@ -50,7 +50,7 @@ def delete_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(o
     return Response(status_code=status.HTTP_204_NO_CONTENT)
     
 @router.put("/{id}", response_model=schemas.PostResponse)
-def update_post(post: schemas.PostCreate, id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def update_post(post: schemas.PostCreate, id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     post_query = db.query(models.Post).where(models.Post.id == id)
     post_to_update = post_query.first()
 
