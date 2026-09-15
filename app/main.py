ba638069ac18560d90407da9c5e0b562ab6a26a0
ThiagoMFC <24575360+ThiagoMFC@users.noreply.github.com>
2026-09-15 11:15:10 -0300

    Add CORS middleware configuration to main application

--- app/main.py
+++ app/main.py
@@ -2,10 +2,24 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
 #create instance of FastAPI named app
 app = FastAPI()
 
+#list of allowed request origins
+origins = ["*"]
+
+#CORS handling
+app.middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#create tables in DB
 models.Base.metadata.create_all(bind=engine)
 
 #import routers
