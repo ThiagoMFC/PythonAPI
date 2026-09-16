6c829b5ddbd2bf5023187a7e1300c1bb7bc3f4f2
ThiagoMFC <24575360+ThiagoMFC@users.noreply.github.com>
2026-09-16 17:13:29 -0300

    Refactor user creation test: set up database connection and override db dependency

--- tests/test_users.py
+++ tests/test_users.py
@@ -2,9 +2,39 @@ from fastapi.testclient import TestClient
 from app.main import app
 from app import schemas
 
-client = TestClient(app)
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from app.config import settings
+from app.database import get_db, Base
+import pytest
 
-def test_create_user():
+
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.DB_USER}:{settings.DB_USER_PWD}@{settings.DB_HOST}/{settings.DB_NAME}_test"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+app.dependency_overrides[get_db] = override_get_db
+
+
+@pytest.fixture
+def client():
+    #drop test db before tests
+    Base.metadata.drop_all(bind=engine)
+    #create new test db
+    Base.metadata.create_all(bind=engine)
+    yield TestClient(app)
+
+def test_create_user(client):
     res = client.post("/users/", json={
         "email": "email123@email.com",
         "password": "pass123"
