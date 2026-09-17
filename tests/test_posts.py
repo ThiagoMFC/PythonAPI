2e26a62acfbb7f485f6e63607d41613c9077617e
ThiagoMFC <24575360+ThiagoMFC@users.noreply.github.com>
2026-09-17 18:02:59 -0300

    Add parameterized test for post creation with title and content assertions

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -36,3 +36,15 @@ def test_get_one_post(authorized_client, test_posts):
 def test_get_one_post_not_exist(authorized_client, test_posts):
     res = authorized_client.get(f"/posts/99999999999999")
     assert res.status_code == 404
+
+@pytest.mark.parametrize("title, content", [
+    ("new title", "new content"),
+    ("another title", "more content")
+])
+def test_create_post(authorized_client, test_user, test_posts, title, content):
+    res = authorized_client.post("/posts/", json={"title": title, "content": content})
+    created_post = schemas.PostResponse(**res.json())
+    assert res.status_code == 201
+    assert created_post.title == title
+    assert created_post.content == content
+    assert created_post.owner.id == test_user["id"]
