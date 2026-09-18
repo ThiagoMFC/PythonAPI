b2d4ced6495bae50241b2021d77b585c3bbf9a0d
ThiagoMFC <24575360+ThiagoMFC@users.noreply.github.com>
2026-09-18 17:33:10 -0300

    Refactor vote creation fixture and remove redundant definition from test_votes.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -124,3 +124,10 @@ def test_posts(test_user, session, test_user2):
     session.commit()
     posts = session.query(models.Post).all()
     return posts
+
+#======================================================== VOTE CREATION FIXTURE ======================================================
+@pytest.fixture
+def test_vote(test_posts, session, test_user):
+    new_vote = models.Vote(post_id=test_posts[3].id, user_id=test_user["id"])
+    session.add(new_vote)
+    session.commit()
--- tests/test_votes.py
+++ tests/test_votes.py
@@ -8,14 +8,6 @@ def test_up_vote(authorized_client, test_posts):
     res = authorized_client.post("/vote/", json={"post_id": test_posts[0].id, "direction": 1})
     assert res.status_code == 201
 
-#fixture to create vote
-@pytest.fixture
-def test_vote(test_posts, session, test_user):
-    new_vote = models.Vote(post_id=test_posts[3].id, user_id=test_user["id"])
-    session.add(new_vote)
-    session.commit()
-
-
 def test_up_vote_twice(authorized_client, test_posts, test_vote):
     res = authorized_client.post("/vote/", json={"post_id": test_posts[3].id, "direction": 1})
     assert res.status_code == 409
