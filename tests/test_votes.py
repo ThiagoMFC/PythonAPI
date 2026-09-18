1c406a7b8a9ce772e473e9a475ed1a301763b853
ThiagoMFC <24575360+ThiagoMFC@users.noreply.github.com>
2026-09-18 17:27:18 -0300

    Rename test for down vote without prior vote and add tests for non-existent posts

--- tests/test_votes.py
+++ tests/test_votes.py
@@ -24,7 +24,7 @@ def test_down_vote(authorized_client, test_posts, test_vote):
     res = authorized_client.post("/vote/", json={"post_id": test_posts[3].id, "direction": 0})
     assert res.status_code == 201
 
-def test_down_vote_not_exist(authorized_client, test_posts):
+def test_down_vote_not_voted(authorized_client, test_posts):
     res = authorized_client.post("/vote/", json={"post_id": test_posts[3].id, "direction": 0})
     assert res.status_code == 404
 
@@ -35,3 +35,11 @@ def test_up_vote_unauth(client, test_posts, test_vote):
 def test_down_vote_unauth(client, test_posts, test_vote):
     res = client.post("/vote/", json={"post_id": test_posts[3].id, "direction": 0})
     assert res.status_code == 401
+
+def test_up_vote_post_not_exist(authorized_client, test_posts):
+    res = authorized_client.post("/vote/", json={"post_id": 99999999999, "direction": 1})
+    assert res.status_code == 404
+
+def test_down_vote_post_not_exist(authorized_client, test_posts):
+    res = authorized_client.post("/vote/", json={"post_id": 99999999999, "direction": 0})
+    assert res.status_code == 404
