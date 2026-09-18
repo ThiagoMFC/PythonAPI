dddbce065fcfa41e63993baa5fa5d3b901a89467
ThiagoMFC <24575360+ThiagoMFC@users.noreply.github.com>
2026-09-18 17:24:28 -0300

    Add tests for down voting functionality and unauthorized access scenarios

--- tests/test_votes.py
+++ tests/test_votes.py
@@ -19,3 +19,19 @@ def test_vote(test_posts, session, test_user):
 def test_up_vote_twice(authorized_client, test_posts, test_vote):
     res = authorized_client.post("/vote/", json={"post_id": test_posts[3].id, "direction": 1})
     assert res.status_code == 409
+
+def test_down_vote(authorized_client, test_posts, test_vote):
+    res = authorized_client.post("/vote/", json={"post_id": test_posts[3].id, "direction": 0})
+    assert res.status_code == 201
+
+def test_down_vote_not_exist(authorized_client, test_posts):
+    res = authorized_client.post("/vote/", json={"post_id": test_posts[3].id, "direction": 0})
+    assert res.status_code == 404
+
+def test_up_vote_unauth(client, test_posts, test_vote):
+    res = client.post("/vote/", json={"post_id": test_posts[3].id, "direction": 1})
+    assert res.status_code == 401
+
+def test_down_vote_unauth(client, test_posts, test_vote):
+    res = client.post("/vote/", json={"post_id": test_posts[3].id, "direction": 0})
+    assert res.status_code == 401
